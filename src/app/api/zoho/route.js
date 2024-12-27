c4bc5111099da6ea4b482a95ba0035f98021ef17
builtbysuraj <surajgupta3553@gmail.com>
2024-12-27 17:23:04 +0530

    Change criteria params

--- src/app/api/zoho/route.js
+++ src/app/api/zoho/route.js
@@ -8,11 +8,8 @@ export const formattedDate = (date) => {
 // Common handler for different HTTP methods
 export async function handler(request) {
   const reqUrl = request.url
-  // const endpointWithQuery = reqUrl.split('?endpoint=')[1];
-  // const endpoint = url.searchParams.get('endpoint')
-  const endpointWithQuery = reqUrl.includes('%') ? decodeURIComponent(reqUrl.split('?endpoint=')[1]) : reqUrl.split('?endpoint=')[1];
-  const endpoint = reqUrl.includes('%') ? decodeURIComponent(url.searchParams.get('endpoint')) : url.searchParams.get('endpoint');
-
+  const endpointWithQuery = reqUrl.split('?endpoint=')[1];
+  const endpoint = url.searchParams.get('endpoint')
 
   const url = new URL(request.url)
   let criteria

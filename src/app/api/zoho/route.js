58000875251f7778d70a4d02d337c72b3137ba52
builtbysuraj <surajgupta3553@gmail.com>
2025-01-07 02:08:50 +0530

    chore: Refine the code

--- src/app/api/zoho/route.js
+++ src/app/api/zoho/route.js
@@ -1,55 +1,51 @@
-import { NextResponse } from 'next/server';
-
-export const formattedDate = (date) => {
-  if (!date) return ''
-  return new Date(date).toISOString().replace('.000Z', '+05:30')
-}
-// const localURLPrefix = "https://mybankauction-backend.vercel.app/api/zoho?endpoint=";
+import { NextResponse } from 'next/server'
 
 function replaceAndEncodeDateInURL(reqUrl) {
-  const baseURL = "https://www.zohoapis.in";
-  // const localURLPrefix = "http://localhost:3000/api/zoho?endpoint=";
-  const localURLPrefix = "https://mybankauction-backend.vercel.app/api/zoho?endpoint=";
+  const baseURL = 'https://www.zohoapis.in'
+  const localURLPrefix =
+    'https://mybankauction-backend.vercel.app/api/zoho?endpoint='
 
+  // const localURLPrefix = 'http://localhost:3000/api/zoho?endpoint='
 
   // Decode the entire URL first
-  const decodedUrl = decodeURIComponent(reqUrl);
+  const decodedUrl = decodeURIComponent(reqUrl)
 
   // Replace the local URL prefix with the base URL
-  let newURL = decodedUrl.replace(localURLPrefix, baseURL);
+  let newURL = decodedUrl.replace(localURLPrefix, baseURL)
 
   // Use regex to find and encode date parts if they exist
-  const startDateRegex = /Auction_start_date:greater_than:([^&]*)/g;
-  const endDateRegex = /Auction_end_date:less_than:([^&]*)/g;
+  const startDateRegex = /Auction_start_date:greater_than:([^&]*)/g
+  const endDateRegex = /Auction_end_date:less_than:([^&]*)/g
 
   if (startDateRegex.test(newURL)) {
     newURL = newURL.replace(startDateRegex, (match, p1) => {
-      return `Auction_start_date:greater_than:${encodeURIComponent(p1)}`;
-    });
+      return `Auction_start_date:greater_than:${encodeURIComponent(p1)}`
+    })
   }
 
   if (endDateRegex.test(newURL)) {
     newURL = newURL.replace(endDateRegex, (match, p1) => {
-      return `Auction_end_date:less_than:${encodeURIComponent(p1)}`;
-    });
+      return `Auction_end_date:less_than:${encodeURIComponent(p1)}`
+    })
   }
 
-  return newURL;
+  return newURL
 }
-
 
 // Common handler for different HTTP methods
 export async function handler(request) {
   const reqUrl = request.url
-  let endpointWithQuery = reqUrl.split('?endpoint=')[1];
-  console.log('...............................................................................................')
+  let endpointWithQuery = reqUrl.split('?endpoint=')[1]
+  console.log(
+    '...............................................................................................'
+  )
   const url = new URL(request.url)
   let criteria
   const endpoint = url.searchParams.get('endpoint')
 
   if (endpoint) {
-    const endpointUrl = new URL(endpoint, url.origin);
-    criteria = endpointUrl.searchParams.get('criteria');
+    const endpointUrl = new URL(endpoint, url.origin)
+    criteria = endpointUrl.searchParams.get('criteria')
   }
 
   if (!endpoint) {
@@ -67,7 +63,6 @@ export async function handler(request) {
     )
   }
 
-  // const zohoUrl = `https://www.zohoapis.in${endpoint}`;
   const zohoUrl = replaceAndEncodeDateInURL(reqUrl)
 
   console.log({ zohoUrl })
@@ -75,10 +70,13 @@ export async function handler(request) {
     const headers = {
       Authorization: accessToken,
       'Content-Type': 'application/json',
-    };
+    }
 
-    let response;
-    const body = (request.method === 'POST' || request.method === 'PUT') ? await request.json() : null;
+    let response
+    const body =
+      request.method === 'POST' || request.method === 'PUT'
+        ? await request.json()
+        : null
 
     // Use switch case for different HTTP methods
     switch (request.method) {
@@ -88,38 +86,61 @@ export async function handler(request) {
           method: request.method,
           headers,
           body: body ? JSON.stringify(body) : null,
-        });
-        break;
+        })
+        break
 
       case 'GET':
-        response = await fetch(zohoUrl, { method: 'GET', headers });
-        break;
+        response = await fetch(zohoUrl, { method: 'GET', headers })
+        break
 
       default:
-        return NextResponse.json({ error: 'Unsupported HTTP method' }, { status: 405 });
+        return NextResponse.json(
+          { error: 'Unsupported HTTP method' },
+          { status: 405 }
+        )
+    }
+    if (response) {
+      return NextResponse({ res: response })
     }
 
     // Handle 204 No Content
     if (response.status === 204) {
-      return new NextResponse(null, { status: 204 }, { url: zohoUrl });
+      return new NextResponse(null, { status: 204 }, { url: zohoUrl })
+    }
+
+    if (response.data[0].code === 'DUPLICATE_DATA') {
+      return NextResponse.json({ message: 'User already exists' })
     }
 
     // Handle unsuccessful responses
     if (!response.ok) {
       // returns if url is wrong
       return NextResponse.json(
-        { error: 'Failed to fetch data from Zoho API', details: response.statusText, url: zohoUrl, url: zohoUrl, endpointWithQuery: endpointWithQuery, endpoint: endpoint, criteria: criteria, reqUrl: reqUrl },
+        {
+          response: response,
+          error: 'Failed to fetch data from Zoho API',
+          details: response.statusText,
+          url: zohoUrl,
+          url: zohoUrl,
+          endpointWithQuery: endpointWithQuery,
+          endpoint: endpoint,
+          criteria: criteria,
+          reqUrl: reqUrl,
+        },
         { status: response.status }
-      );
+      )
     }
 
     // Parse JSON data (if not 204)
-    let data = null;
+    let data = null
     if (response.status !== 204) {
       try {
-        data = await response.json();
+        data = await response.json()
       } catch (jsonError) {
-        return NextResponse.json({ error: 'Failed to parse JSON response', url: zohoUrl }, { status: 500 });
+        return NextResponse.json(
+          { error: 'Failed to parse JSON response', url: zohoUrl },
+          { status: 500 }
+        )
       }
     }
 
@@ -128,13 +149,22 @@ export async function handler(request) {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
       },
-    });
-
+    })
   } catch (error) {
     // Catch any unexpected errors and return to the client
-    return NextResponse.json({ error: 'Internal Server Error', details: error, url: zohoUrl, endpointWithQuery: endpointWithQuery, endpoint: endpoint, criteria: criteria, reqUrl: reqUrl }, { status: 500 });
+    return NextResponse.json(
+      {
+        error: 'Internal Server Error',
+        details: error,
+        url: zohoUrl,
+        endpointWithQuery: endpointWithQuery,
+        endpoint: endpoint,
+        criteria: criteria,
+        reqUrl: reqUrl,
+      },
+      { status: 500 }
+    )
   }
-
 }
 
 // Expose methods individually
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,7 @@
-'use client'
-
 export default function Home() {
-  const signupApi = async () => {
-    const res = await fetch('https://www.zohoapis.in/crm/v7/Accounts/search?criteria=Reserve_price:greater_than:0(Auction_end_date%3Aless_than%3A2025-01-03T18%3A30%3A00%2B05%3A30)&page=1&per_page=300', {
-      headers: {
-        Authorization:
-          'Zoho-oauthtoken 1000.a8f2ffb710311899b1a58563d982c038.6bcdd95ed4c1dc0420c60014a5c6b62b',
-        'Content-Type': 'application/json',
-      }
-    })
-    const data = await res.json()
-    console.log(data)
-  }
-
   return (
     <div>
-      <button onClick={signupApi}>Click for magic</button>
+      <h1>Server is running</h1>
     </div>
   )
 }
